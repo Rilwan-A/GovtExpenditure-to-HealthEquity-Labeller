64bbe56d9ba0f419f8e9834f12c421701bbbfd91
AkanniAde <rilwan.ade1@gmail.com>
2023-09-27 00:32:46 +0100

    debugging calibration script

--- agent_based_modelling/calibration.py
+++ agent_based_modelling/calibration.py
@@ -8,20 +8,24 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import argparse 
-import ppi
+from agent_based_modelling import ppi
 from prompt_engineering.utils import ALL_MODELS
 import yaml
 warnings.simplefilter("ignore")
 import time
+import glob
+from collections import defaultdict
 
-from i2i_edge_estimation.create_i2i_candidates import concatenate_name_age
+from agent_based_modelling.i2i_edge_estimation.create_i2i_candidates import concatenate_name_age
+
 from prompt_engineering.my_logger import setup_logging_calibration
 logging = None
+from builtins import FileNotFoundError
 
 #TODO: ensure the b2i method is handled e.g. figure out what edits to do in order to get the b2i matrix
 
-def main(start_year, end_year, parallel_processes, experiment_mode,
-            b2i_method, i2i_method, i2i_model_size,
+def main(start_year, end_year, parallel_processes,
+            b2i_method, i2i_method, model_size,
             thresholds:list[float], low_precision_counts, increment,
             debugging, time_experiments=False,
             exp_samples=1,
@@ -40,17 +44,16 @@ def main(start_year, end_year, parallel_processes, experiment_mode,
     logging.info(f"\tincrement: {increment}")
     logging.info(f"\tb2i_method: {b2i_method}")
     logging.info(f"\ti2i_method: {i2i_method}")
-    logging.info(f"\ti2i_model_size: {i2i_model_size}")
-    logging.info(f"\texperiment_mode: {experiment_mode}")   
+    logging.info(f"\tmodel_size: {model_size}")
     logging.info(f"\tverbose: {verbose}")
     logging.info(f"\ttime_experiments: {time_experiments}")
 
     # Get calibration kwargs
     calibration_kwargs = get_calibration_kwargs(
                                                 b2i_method,
-                                                i2i_method,i2i_model_size,
+                                                i2i_method,model_size,
                                                 start_year, end_year, 
-                                                experiment_mode )
+                                                )
 
     exp_dir = os.path.join('.','agent_based_modelling','output', 'ppi_calibration' )
 
@@ -98,8 +101,6 @@ def main(start_year, end_year, parallel_processes, experiment_mode,
             df_time = pd.DataFrame({'time_elapsed':time_elapsed, 'iterations':iterations, 'exp_number':list(range(exp_samples))})
             df_time.to_csv(os.path.join(exp_dir, exp_number, f'calibration_time.csv'), index=False)
 
-
-
         # Save hyperparameters as yaml
         experiment_hyperparams = {
             'start_year':start_year,
@@ -110,8 +111,7 @@ def main(start_year, end_year, parallel_processes, experiment_mode,
             'increment':increment,
             'b2i_method':b2i_method,
             'i2i_method':i2i_method,
-            'i2i_model_size':i2i_model_size,
-            'experiment_mode':experiment_mode,
+            'model_size':model_size,
             'exp_number':exp_number,
             'time_experiments':time_experiments
         }
@@ -123,13 +123,13 @@ def main(start_year, end_year, parallel_processes, experiment_mode,
 
 def get_calibration_kwargs(
                         b2i_method,
-                        i2i_method, i2i_model_size,
+                        i2i_method, model_size,
                         start_year=None,
                            end_year=None,
                             ):
 
     # TODO: need to recreate pipeline_indicators_normalized file (rl,R,successRates) are based on test period including 2019
-    df_indic = pd.read_csv('./data/ppi/pipeline_indicators_normalized_2013_2016.csv', encoding='unicode_escape') 
+    df_indic = pd.read_csv('./data/ppi/pipeline_indicators_normalized_finegrained.csv', encoding='utf-8') 
     colYears = [col for col in df_indic.columns if str(col).isnumeric()]
 
     # TODO: need to create a pipeline_expenditure_finegrained which focuses on the fine grained budget items
@@ -175,13 +175,12 @@ def get_calibration_kwargs(
 
     # TODO: need to create a pipeline_relation_table which focuses on relationships between finegrained budget items and indicators
     # TODO: this also needs to be dependent on the b2i method being used
-    df_rela = pd.read_csv(os.path.join('agent_based_modelling','data','pipeline_relation_table.csv'))
+    
 
-    B_dict = {} # PPI needs the relational table in the form of a Python dictionary
-    for index, row in df_rela.iterrows():
-        B_dict[int(row.indicator_index)] = [int(programme) for programme in row.values[1::][row.values[1::].astype(str)!='nan']]
+    b2i_network = get_b2i_network( b2i_method, model_size )
 
-    i2i_network = get_i2i_network( i2i_method=i2i_method, i2i_model_size=i2i_model_size, indic_count=indic_count )
+    # Load in the i2i relation table
+    i2i_network = get_i2i_network( i2i_method=i2i_method, model_size=model_size, indic_count=indic_count )
 
     return {
         'indic_start': indic_start,
@@ -191,22 +190,70 @@ def get_calibration_kwargs(
         'qm': qm,
         'rl': rl,
         'Bs': Bs,
-        'B_dict': B_dict,
+        'B_dict': b2i_network,
         'T': T,
         # 'indic_count': indic_count,
         # 'indis_index': indis_index,
         'i2i_network':i2i_network
     }
 
-def get_i2i_network(i2i_method, indic_count, i2i_model_size=None):
+def get_b2i_network(b2i_method,  model_size) -> dict[int, list[int]]:
+    # Create a dictionary which aligns indicators with budget items
+    # Both indicators and budget items are referred to by their index in data_expenditure_trend_finegrained and data_expenditure_raw respectively
+    # B = { 'idic_idx0':[bi_idx0, bi_idx1, ... ] }
 
-    assert i2i_method in ['bdag','CPUQ','verbalize','entropy','zero'], f'Spillover Creator Model name {model_name} not recognised'
+    if b2i_method == 'ea':
+        # Load in the b2i relation table
+        df_rela = pd.read_csv(os.path.join('data','ppi','pipeline_relation_table_finegrained.csv'))
+
+        B_dict = {} # PPI needs the relational table in the form of a Python dictionary
+        for index, row in df_rela.iterrows():
+            B_dict[int(row.indicator_index)] = [int(programme) for programme in row.values[1::][row.values[1::].astype(str)!='nan']]
+
+    elif b2i_method in ['verbalize', 'CPUQ_binomial']:
+        
+        # Load the varbalize
+        if b2i_method == 'verbalize':
+            dir_ = os.path.join('prompt_engineering', 'output', 'spot', f'ppi_b2i_{model_size}_verbalize')
+        elif b2i_method == 'CPUQ_binomial':
+            dir_ = os.path.join('prompt_engineering', 'output', 'spot', f'ppi_b2i_{model_size}_cpuq')
+
+        _ =  glob.glob(os.path.join(dir_, '**' ,'**predictions_b2i.csv')) # (budget_item,indicator, related, pred_aggregated, prompts, predictions, discourse)
+        if len(_) == 0:
+            raise FileNotFoundError(f'No b2i predictions found at {dir_}')
+        b2i_preds = pd.read_csv(_[0])
+        
+        # load info on the index ordering of indicators
+
+        # convert to a dictionary where the key is the indicator_name and the value is the indicator's index
+        indicator_ref = pd.read_csv(os.path.join('data','ppi','pipeline_indicators_normalized_finegrained.csv'), usecols=['indicator_name'])
+        dict_indic_idx = {v: k for k, v in indicator_ref['indicator_name'].to_dict().items()}
+
+        # contains info on the index ordering of budget items 
+        budget_item_ref = pd.read_csv(os.path.join('data','ppi','data_expenditure_trend_finegrained.csv'), usecols=['seriesName'])
+        dict_bi_idx = {v: k for k, v in budget_item_ref['seriesName'].to_dict().items()}
+
+        B_dict = defaultdict(list) # PPI needs the relational table in the form of a Python dictionary
+        for _, row in b2i_preds.iterrows():
+            
+            bi_idx = dict_bi_idx[ row.budget_item ] # get the index of the indicator
+            indic_idx = dict_indic_idx[ row.indicator]
+            # value in dict @idx 249 'Proportion of children aged 2-2Ã\x83Â\x82Ã\x82Â½yrs receiving ASQ-3 as part of the Healthy Child Programme or integrated review'
+            # value in dict_indic_idx
+            B_dict[indic_idx].append(bi_idx)
+
+    return B_dict
+
+def get_i2i_network(i2i_method, indic_count, model_size=None):
+    # Creates an array representing indicator to indicator relationships
+
+    assert i2i_method in ['ccdr','CPUQ_multinomial','verbalize','entropy','zero'], f'Spillover Creator Model name {model_name} not recognised'
     
     if i2i_method == 'zero':
         i2i_network = np.zeros((indic_count, indic_count))
 
-    elif i2i_method == 'bdag':
-        _path = os.path.join('data','ppi','i2i_networks','bdag.csv')
+    elif i2i_method == 'ccdr':
+        _path = os.path.join('data','ppi','i2i_networks','ccdr.csv')
         if os.path.exists(_path):
             df_net = pd.read_csv(_path)
         else:
@@ -219,7 +266,7 @@ def get_i2i_network(i2i_method, indic_count, i2i_model_size=None):
             w = row.Weight
             i2i_network[i,j] = w
 
-    elif i2i_method in [ 'CPUQ', 'verbalize', 'entropy']:
+    elif i2i_method in [ 'CPUQ_multinomial', 'verbalize', 'entropy']:
 
         # Reading in the i2i network
         _1 = {
@@ -231,49 +278,37 @@ def get_i2i_network(i2i_method, indic_count, i2i_model_size=None):
         _2 = {'CPUQ':'mn', 'verbalize':'verb', 'entropy':'ent'}
         
 
-        _path = os.path.join('prompt_engineering','output', 'spot', _1[i2i_model_size], 
+        _path = os.path.join('prompt_engineering','output', 'spot', _1[model_size], 
             f'i2i_{_2[i2i_method]}_weights.csv')
         
         if os.path.exists(_path):
             df_net = pd.read_csv(_path) # columns = ['indicator1', 'indicator2', 'weight']
+            # filtering out rows where no weight predict or weight was None
+            df_net = df_net[ ~df_net.weight.isnull() ]
+            df_net.weight =  df_net.weight.apply(lambda x: eval(x))
+
         else:
             raise FileNotFoundError(f'i2i Network not created - no file at {_path}')
         
-        # Establishing the indices for the indicators and budget items
+        # Establishing the indices for the indicators
         # The order can be found from the pipeline_indicators_normalized_2013_2016.csv file
-        try:
-            df_index_indic = pd.read_csv('./data/ppi/pipeline_indicators_normalized_2013_2016.csv', usecols=['idx','indicator_name_ftmd'] )
-            # catch exception of cols in use_cols not present
-        except ValueError:
-            # run a python file
-            logging.info("Pipeline indicators file `/pipeline_indicators_normalized_2013_2016` does not have columns 'idx' and 'indicator_name_fmtd'")
-            logging.info("Running create_indicators_index.py to add the columns to the file")
-            import subprocess
-            subprocess.run(['python3', './agent_based_modelling/i2i_edge_estimation/create_indicators_index.py'])
-            df_index_indic = pd.read_csv('./data/ppi/pipeline_indicators_normalized_2013_2016.csv', usecols=['idx','indicator_name_ftmd'] )
 
-        # dict connecting the index to the indicator name (formatted)
-        dict_indicators_idx = df_index_indic.set_index('indicator_name_fmtd')['idx'].to_dict()
+        # convert to a dictionary where the key is the indicator_name and the value is the indicator's index
+        indicator_ref = pd.read_csv(os.path.join('data','ppi','pipeline_indicators_normalized_finegrained.csv'), usecols=['indicator_name'])
+        dict_indic_idx = {v: k for k, v in indicator_ref['indicator_name'].to_dict().items()}
 
         i2i_network = np.zeros((indic_count, indic_count)) # adjacency matrix
+        
         for index, row in df_net.iterrows():
             
-            # Checking if edge exists
-            edge_exists = row.weight is not 'None'
-            if edge_exists is False:
+            
+            weight = row.get('scale_mean', row.get('mean', 0.0))
+            
+            if weight == 0.0:
                 continue
-            
-            i = dict_indicators_idx[row.indicator1] 
-            j = dict_indicators_idx[row.indicator2] 
-            
-            weight_dict = eval(row.weight)
 
-            if 'mean_scaled' in weight_dict.keys():
-                weight = weight_dict['mean_scaled']
-            elif 'mean' in weight_dict.keys():
-                weight = weight_dict['mean']
-            else:
-                raise ValueError('No weight found in weight_dict')
+            i = dict_indic_idx[row.indicator1] 
+            j = dict_indic_idx[row.indicator2] 
             
             i2i_network[i,j] = weight
             
@@ -286,15 +321,6 @@ def calibrate(indic_start, indic_final, success_rates, R, qm, rl, Bs, B_dict, T,
               verbose=True, 
               time_experiments=False):
 
-    # if parallel_processes == None:
-    #     parallel_processes =  6 # number of cores to use
-    # if threshold == None:
-    #     threshold =  0.8 # the quality of the calibration (I choose a medium quality for illustration purposes)
-    # if low_precision_counts == None:
-    #     low_precision_counts =  75  # number of low-quality iterations to accelerate the calibration
-
-
-    
     dict_output = ppi.calibrate(indic_start, indic_final, success_rates, so_network=i2i_network, R=R, qm=qm, rl=rl, Bs=Bs, B_dict=B_dict,
                 T=T, threshold=threshold, parallel_processes=parallel_processes, verbose=True,
                 low_precision_counts=low_precision_counts,
@@ -313,15 +339,14 @@ def get_args():
     parser.add_argument('--low_precision_counts', type=int, default=75, help='Number of low-quality iterations to accelerate the calibration')
     parser.add_argument('--increment', type=int, default=100, help='Number of iterations between each calibration check')
 
-    parser.add_argument('--b2i_method', type=str, default='bdag', choices=['naive','verbalize','zero'], help='Name of the spillover predictor model')
-    parser.add_argument('--i2i_method', type=str, default='bdag', choices=['bdag','verbalize','CPUQ', 'zero', 'entropy'], help='Name of the indicator to indicator edge predictor method')
-    parser.add_argument('--i2i_model_size', type=str, default='7bn', choices=['7bn','13bn','30bn' ], help='Name of the indicator to indicator edge predictor method')
+    parser.add_argument('--b2i_method', type=str, default='ccdr', choices=[ 'ea', 'verbalize', 'CPUQ_binomial' ], help='Name of the spillover predictor model')
+    parser.add_argument('--i2i_method', type=str, default='ccdr', choices=['ccdr', 'CPUQ_multinomial', 'zero', 'entropy'], help='Name of the indicator to indicator edge predictor method')
+    parser.add_argument('--model_size', type=str, default='7bn', choices=['7bn','13bn','30bn' ], help='Name of the indicator to indicator edge predictor method')
     
-    parser.add_argument('--experiment_mode', type=str, choices=[ 'calibrate' ])
     parser.add_argument('--verbose', action='store_true', default=False, help='Print progress to console')
     parser.add_argument('--time_experiments', action='store_true', default=False, help='Record Calibration Time for Experiments')
     parser.add_argument('--exp_samples',type=int, default=1, help='Number of samples to take for time experiments')
-    parser.add_argument('--debugging', action='store_true',, default=False)
+    parser.add_argument('--debugging', action='store_true', default=False)
     args = parser.parse_args()
     return args
 
